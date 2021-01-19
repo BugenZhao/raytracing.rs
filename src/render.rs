d1862d5065314bd0a2992331217f332fd1b4ebb9
Bugen Zhao <i@bugenzhao.com>
2021-01-19 14:10:57 +0000

    render: minor changes

--- src/render.rs
+++ src/render.rs
@@ -4,6 +4,12 @@ use rayon::prelude::*;
 
 use crate::{output::output_png, scene, vec3::RelColor};
 
+const MAX_DEPTH: usize = 50;
+// antialiasing
+const SAMPLES_PER_AXIS: u32 = 10;
+const SAMPLES_PER_PIXEL: u32 = SAMPLES_PER_AXIS * SAMPLES_PER_AXIS;
+const SAMPLE_STEP: f64 = 1f64 / SAMPLES_PER_AXIS as f64;
+
 pub fn render() -> Result<()> {
     let scene = scene::examples::simple_scene();
     let aspect_ratio = scene.camera.aspect_ratio;
@@ -11,27 +17,22 @@ pub fn render() -> Result<()> {
     let width: u32 = 800;
     let height: u32 = (width as f64 / aspect_ratio) as u32;
 
-    const MAX_DEPTH: usize = 50;
-
-    // antialiasing
-    const SAMPLES_PER_AXIS: u32 = 10;
-    const SAMPLES_PER_PIXEL: u32 = SAMPLES_PER_AXIS * SAMPLES_PER_AXIS;
-    const SAMPLE_STEP: f64 = 1f64 / SAMPLES_PER_AXIS as f64;
-
     let data: Vec<u8> = iproduct!((0..height).rev(), (0..width))
         .collect::<Vec<_>>()
         .into_par_iter()
         .flat_map(|(j, i)| {
-            let mean_rel_color: RelColor = iproduct!((0..SAMPLES_PER_AXIS), (0..SAMPLES_PER_AXIS))
-                .fold(RelColor::zeros(), |color, (a, b)| {
+            (iproduct!((0..SAMPLES_PER_AXIS), (0..SAMPLES_PER_AXIS)).fold(
+                RelColor::zeros(),
+                |color, (a, b)| {
                     let u = (i as f64 + (a as f64 + 0.5) * SAMPLE_STEP) / (width - 1) as f64;
                     let v = (j as f64 + (b as f64 + 0.5) * SAMPLE_STEP) / (height - 1) as f64;
                     let ray = scene.camera.ray(u, v);
 
-                    color + (scene.world.rel_color_of(&ray, MAX_DEPTH) / SAMPLES_PER_PIXEL as f64)
-                });
-
-            mean_rel_color.into_8bit_color().into_vec()
+                    color + scene.world.rel_color_of(&ray, MAX_DEPTH)
+                },
+            ) / SAMPLES_PER_PIXEL as f64)
+                .into_8bit_color()
+                .into_vec()
         })
         .collect();
 
